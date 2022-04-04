2ed6115a614b619a9caedea3db864ba467ca97f1
Ben Johnson <72088635+BenJohnson0@users.noreply.github.com>
2022-04-04 15:42:03 +0100

    Random diagrams created using JFreeChart
    Comments added and changed the look of the GUI with colours and fonts

--- Java_Assignment/src/Data_Analysis/MyDataGUI.java
+++ Java_Assignment/src/Data_Analysis/MyDataGUI.java
@@ -10,7 +10,9 @@
 
 package Data_Analysis;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -28,15 +30,22 @@ import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 import org.jfree.chart.ChartUtilities;
-import org.jfree.chart.ChartFactory; 
-import org.jfree.chart.JFreeChart; 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
+
+
+import java.util.Random;
+
 
 public class MyDataGUI extends JFrame implements ActionListener
 {
 
 	private static final long serialVersionUID = 1L;
-	//attributes
+	
+	//  attributes
 	JTextField year_entry, education_entry, sex_entry, query_entry, SQL_entry;
 	JButton displayButton, SQLButton, RandButton;
 	JPanel MainPanel, displayPanel, SQLPanel, RandPanel;
@@ -51,19 +60,25 @@ public class MyDataGUI extends JFrame implements ActionListener
 	int index = 0;
 	
 	String sex, ed, quoted_sex_entry, quoted_education_entry;
-	//for JButton
+	
+	//  font styles
+	Font  f4  = new Font(Font.SANS_SERIF, Font.BOLD|Font.ITALIC, 15);
+	Font  f1  = new Font(Font.SERIF, Font.PLAIN,  15);
+	
+	/*for JButton
 	public static final String[] years = new String[] {"2009", "2010",  "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"};
 	public static final String[] levels = new String[] {"Primary", "Lower Secondary", "Upper Secondary", "Leaving certificate", "Third level", "Higher certificate", "Ordinary bachelor", "Honours bachelor", "Postgradute"};
 	public static final String[] sexes = new String[] {"Male", "Female", "Both"};
-
+	*/
 	
 	//	MyDataGUI constructor
 	public MyDataGUI(String title)
 	{
+
 		//	layout
 		super(title);
 		setVisible(true);
-		setSize(800, 300);
+		setSize(600, 150);
 		setLayout(new FlowLayout());
 		
 		//	create panel
@@ -73,10 +88,12 @@ public class MyDataGUI extends JFrame implements ActionListener
 		
 		//	year label
 		yearLabel = new JLabel("Year:");
+		yearLabel.setFont(f1);
 		MainPanel.add(yearLabel);
 		
 		//	year textField
 		year_entry = new JTextField("Enter year");
+		year_entry.setFont(f1);
 		year_entry.setToolTipText("Select a year between 2009 and 2021");
 		MainPanel.add(year_entry);
 
@@ -89,10 +106,12 @@ public class MyDataGUI extends JFrame implements ActionListener
 		
 		//	sex label
 		sexLabel = new JLabel("Sex:");
+		sexLabel.setFont(f1);
 		MainPanel.add(sexLabel);
 		
 		//	sex textField
 		sex_entry = new JTextField("Enter sex");
+		sex_entry.setFont(f1);
 		sex_entry.setToolTipText("Select Male, Female or Both");
 		MainPanel.add(sex_entry);
 		
@@ -105,10 +124,12 @@ public class MyDataGUI extends JFrame implements ActionListener
 		
 		//	education label
 		educationLabel = new JLabel("Education Level:");
+		educationLabel.setFont(f1);
 		MainPanel.add(educationLabel);
 		
 		//	education textField
 		education_entry = new JTextField("Enter education");
+		education_entry.setFont(f1);
 		education_entry.setToolTipText("Select Primary, Lower/Upper Secondary, Leaving certificate, Third level, Higher certificate, Ordinary bachelor, Honours bachelor or Postgradute");
 		MainPanel.add(education_entry);
 		
@@ -121,6 +142,8 @@ public class MyDataGUI extends JFrame implements ActionListener
 		
 		//	confirm entries		
 		displayButton = new JButton("Display data");
+		displayButton.setBackground(Color.orange);
+		displayButton.setFont(f4);
 		MainPanel.add(displayButton);
 		displayButton.setToolTipText("Display data for specified terms");
 		displayButton.addActionListener(this);
@@ -133,40 +156,46 @@ public class MyDataGUI extends JFrame implements ActionListener
 		SQLPanel.isVisible();
 		
 		SQLLabel = new JLabel("SQL:");
+		SQLLabel.setFont(f1);
 		SQLPanel.add(SQLLabel);
 		
 		SQL_entry = new JTextField("select * from EducationData");
+		SQL_entry.setFont(f1);
 		SQL_entry.setToolTipText("Create your own SQL query here");
 		SQLPanel.add(SQL_entry);
 		
 		SQLButton = new JButton("Submit");
+		SQLButton.setBackground(Color.pink);
+		SQLButton.setFont(f4);
 		SQLPanel.add(SQLButton);
 		SQLButton.setToolTipText("Submit your SQL query");
 		SQLButton.addActionListener(this);
 		
 		// random button
-		RandButton = new JButton("Random Fact");
+		RandButton = new JButton("Random Diagram");
+		RandButton.setBackground(Color.LIGHT_GRAY);
+		RandButton.setFont(f4);
 		SQLPanel.add(RandButton);
-		RandButton.setToolTipText("Click here to get a random fact about the data!");
+		RandButton.setToolTipText("Click here to get a random diagram about the data!");
 		RandButton.addActionListener(this);
 		
                 
-		}//close MyDataGUI constructor
+		}//  close MyDataGUI constructor
 				
 		public void actionPerformed(ActionEvent e) 
 		{
 			
 			Object obj = e.getSource();
 			
-			// button for user input SQL
+			//  button for user input SQL
 			if(obj == SQLButton) {
 				
 				try
 				{
-					Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");   // load the driver
+					Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");   //  load the driver
 				}
 				   
-					//Catch driver loading errors
+					//  Catch driver loading errors
 					catch (ClassNotFoundException f)
 					{
 						System.out.println ("Could not load the driver.");
@@ -174,30 +203,30 @@ public class MyDataGUI extends JFrame implements ActionListener
 
 				try {
 					
-				//URL for connection to the database
+				//  URL for connection to the database
 				String url = "jdbc:sqlserver://localhost;Database=JavaAssignment;IntegratedSecurity=true;encrypt=true;trustServerCertificate=true";
 
-				//Register the driver
+				//  Register the driver
 				DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
 
-				// Connecting to the database
+				//  Connecting to the database
 				Connection conn = DriverManager.getConnection(url);
 				System.out.println ("\n\n- - - - - - - - - - - -");
 				System.out.println ("Connection successful");
 				System.out.println ("- - - - - - - - - - - -\n\n");
 				System.out.println("Your Query: " +SQL_entry.getText()+ "\n");
 
-				//Create statement object for running SQL clauses
+				//  Create statement object for running SQL clauses
 				Statement stmt = conn.createStatement ();  
 
-				//Query query_entry.getText(), education_entry.getText(), year_entry.getText(), sex_entry.getText() 
+				//  Query query_entry.getText(), education_entry.getText(), year_entry.getText(), sex_entry.getText() 
 				ResultSet rset = stmt.executeQuery (SQL_entry.getText());
 				
-				//create table for displaying results in the GUI
+				//  create table for displaying results in the GUI
 				String columns[] = { "Year", "Sex", "Education Level", "Unemployment Rate" };
 			    String data[][] = new String[360][4];
 			    
-			    //loop through the results and populate the JTable
+			    //  loop through the results and populate the JTable
 			    int i = 0;
 			    while (rset.next()) {
 			        String t_year = rset.getString("d_Year");
@@ -209,9 +238,9 @@ public class MyDataGUI extends JFrame implements ActionListener
 			        data[i][2] = t_ed;
 			        data[i][3] = t_val;
 			        i++;
-			    }//close while
+			    }//  close while
 			    
-			    //create JTable for the data
+			    //  create JTable for the data
 			    DefaultTableModel model = new DefaultTableModel(data, columns);
 			    JTable table = new JTable(model);
 			    table.setShowGrid(true);
@@ -227,16 +256,16 @@ public class MyDataGUI extends JFrame implements ActionListener
 			    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			    f.setVisible(true);
 				  
-				stmt.close();  // Close the statement object
-				conn.close();   // Close the connection
-				}//close try
+				stmt.close();  //  Close the statement object
+				conn.close();   //  Close the connection
+				}//  close try
 				   
 				catch(SQLException g)
 				{
 					System.err.print("SQLException: ");
 					System.err.println(g.getMessage());
-				}//close catch
-			}//end SQLButton
+				}//  close catch
+			}//  end SQLButton
 			
 			
 		 
@@ -244,9 +273,10 @@ public class MyDataGUI extends JFrame implements ActionListener
 			
 			
 			
-			// button for set queries
+			//  button for set queries
 			else if(obj == displayButton) {
 				
+			//  add single quotes to strings so they can be executed in SQL
 			quoted_sex_entry = "\'" + sex_entry.getText() + "\'";
 			quoted_education_entry = "\'" + education_entry.getText() + "\'";
 			
@@ -257,13 +287,13 @@ public class MyDataGUI extends JFrame implements ActionListener
 				System.exit(1);
 			*/
 		
-			//Load Driver
+			//  Load Driver
 			try
 			{
 				Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");   // load the driver
 			}
 			   
-			//Catch driver loading errors
+			//  Catch driver loading errors
 			catch (ClassNotFoundException g)
 			{
 				System.out.println ("Could not load the driver.");
@@ -271,41 +301,43 @@ public class MyDataGUI extends JFrame implements ActionListener
 
 			try {
 				
-			//URL for connection to the database
+			//  URL for connection to the database
 			String url = "jdbc:sqlserver://localhost;Database=JavaAssignment;IntegratedSecurity=true;encrypt=true;trustServerCertificate=true";
 
-			//Register the driver
+			//  Register the driver
 			DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());   
 
-			// Connecting to the database
+			//  Connecting to the database
 			Connection conn = DriverManager.getConnection(url);
 			System.out.println ("\n\n- - - - - - - - - - - -");
 			System.out.println ("Connection successful");
 			System.out.println ("- - - - - - - - - - - -\n");
 
-			//Create statement object for running SQL clauses
+			//  Create statement object for running SQL clauses
 			Statement stmt = conn.createStatement ();  
 
-			//Query query_entry.getText(), education_entry.getText(), year_entry.getText(), sex_entry.getText() 
+			//  create result set for SQL results
 			ResultSet rset = stmt.executeQuery ("select * from EducationData where d_Year = " +year_entry.getText()+ 
 					"and d_Sex = " +quoted_sex_entry+ "and d_EducationLevel = " +quoted_education_entry);
    
+			//  Display the data
 			while (rset.next()) 
 			{
-				JOptionPane.showMessageDialog(frame, rset.getString(4)+ "% Unemployment rate.");
+				JOptionPane.showMessageDialog(frame, education_entry.getText() + ", " + sex_entry.getText()+ ", " 
+						+ year_entry.getText()+ ": " +rset.getString(4)+ "% unemployment rate");
 			}
 			   
-			stmt.close();  // Close the statement object
-			conn.close();   // Close the connection
-			}//close try
+			stmt.close();  //  Close the statement object
+			conn.close();   //  Close the connection
+			}//  close try
 			   
 				catch(SQLException g)
 				{
 					System.err.print("SQLException: ");
 					System.err.println(g.getMessage());
-				}//close catch
+				}//  close catch
 
-			}//close displayButton
+			}//  close displayButton
 			
 			
 		
@@ -315,12 +347,18 @@ public class MyDataGUI extends JFrame implements ActionListener
 		
 		if(obj == RandButton) {
 			
+			//  generate a random number after each click 
+			Random rand = new Random();
+			int number = rand.nextInt(3);
+			number += 1;
+		
+			
 			try
 			{
-				Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");   // load the driver
+				Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");   //  load the driver
 			}
 			   
-				//Catch driver loading errors
+				//  Catch driver loading errors
 				catch (ClassNotFoundException f)
 				{
 					System.out.println ("Could not load the driver.");
@@ -328,60 +366,150 @@ public class MyDataGUI extends JFrame implements ActionListener
 
 			try {
 				
-			//URL for connection to the database
+			//  URL for connection to the database
 			String url = "jdbc:sqlserver://localhost;Database=JavaAssignment;IntegratedSecurity=true;encrypt=true;trustServerCertificate=true";
 
-			//Register the driver
+			//  register the driver
 			DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
 
-			// Connecting to the database
+			//  connecting to the database
 			Connection conn = DriverManager.getConnection(url);
 			System.out.println ("\n\n- - - - - - - - - - - -");
 			System.out.println ("Connection successful");
 			System.out.println ("- - - - - - - - - - - -\n\n");
 
-			//Create statement object for running SQL clauses
+			//  Create statement object for running SQL clauses
 			Statement stmt = conn.createStatement ();  
-
-			//Query query_entry.getText(), education_entry.getText(), year_entry.getText(), sex_entry.getText() 
+			
+			//  create ResultSet from Database
 			ResultSet rset = stmt.executeQuery ("SELECT * from EducationData");
 			
+			if (number == 1) {
+			
+			//  create dataset 
 			DefaultPieDataset dataset = new DefaultPieDataset();
 		      
+			//  load data into dataset
 		      while(rset.next()) {
 		         dataset.setValue ( 
-		         rset.getString("d_Sex") ,
+		         rset.getString("d_EducationLevel") ,
 		         Integer.parseInt(rset.getString("d_Value")));
 		      }
 		      
+		      //  populate diagram
 		      JFreeChart chart = ChartFactory.createPieChart(
-		    	         "Sex VS Value",   // chart title           
-		    	         dataset,          // data           
-		    	         true,             // include legend          
+		    	         "Education Level VS Unemployment Rate",              
+		    	         dataset,                     
+		    	         true,                      
 		    	         true,           
 		    	         false );
 		      
-		      int width = 560;    /* Width of the image */
-		      int height = 370;   /* Height of the image */ 
+		      int width = 1000;     
+		      int height = 700;     
 		      File pieChart = new File( "Pie_Chart.jpeg" );
+		      
+		      //  save pie chart to PC
 		      try {
-				ChartUtilities.saveChartAsJPEG(pieChart, chart, width, height);
-		      } 
+				ChartUtilities.saveChartAsPNG(pieChart, chart, width, height);
+				JOptionPane.showMessageDialog(frame, "Saved pie chart to PC!");
+		      }//  close try 
+		      
 		      catch (IOException e1) 
 		      {
 				e1.printStackTrace();
+		      }//  close catch
+		       
+			}//  close if number == 1
+		      
+			
+			if (number == 2) {
+			
+			//  create dataset
+			DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+			
+			//  load values into dataset
+			while(rset.next()) {
+		         dataset.setValue ( 
+		         Integer.parseInt(rset.getString("d_Value")), 
+		         rset.getString("d_EducationLevel") , rset.getString("d_Sex")
+		         );
 		      }
-
-			}
+		      
+			//  create bar chart
+			JFreeChart barChart = ChartFactory.createBarChart(
+			         getTitle(),
+			         "Gender",
+			         "Unemployment Rate",
+			         dataset,
+			         PlotOrientation.VERTICAL,
+			         true,true,false);
+			         
+				int width = 1000;    //  Width of the image 
+				int height = 700;   //  Height of the image  
+				File BarChart = new File( "Bar_Chart.jpeg" );
+				
+			  //  save bar chart to PC
+		      try {
+				ChartUtilities.saveChartAsPNG(BarChart, barChart, width, height);
+				JOptionPane.showMessageDialog(frame, "Saved bar chart to PC!");
+		      }//  close try 
+		      
+		      catch (IOException e1) 
+		      {
+				e1.printStackTrace();
+		      }//  close catch
+		      
+			}//  close if number == 2
+			
+			//  Line chart diagram
+			if (number == 3) {
+				
+				//  create dataset
+				DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+				
+				//  load values into dataset
+				while(rset.next()) {
+			         dataset.setValue ( 
+			         Integer.parseInt(rset.getString("d_Value")), 
+			         rset.getString("d_Sex") , rset.getString("d_Year")
+			         );
+			      }//  close while
+				
+				//  create line graph
+				JFreeChart lineChart = ChartFactory.createLineChart(
+				         getTitle(),
+				         "Years","Unemployment Rate",
+				         dataset,
+				         PlotOrientation.VERTICAL,
+				         true,true,false);
+				         
+					int width = 1000;    //  width of the image 
+					int height = 700;    //  height of the image  
+					File LineChart = new File("Line_Chart.jpeg");
+					
+					//  save complete diagram to PC
+			      try {
+					ChartUtilities.saveChartAsPNG(LineChart, lineChart, width, height);
+					JOptionPane.showMessageDialog(frame, "Saved line chart to PC!");
+			      }//  close try
+			      
+			      		catch (IOException e1) 
+			      		{
+			      			e1.printStackTrace();
+			      		}//  close catch
+				}//  close if number == 3
+			      
+			}//  close try RandomButton
 			
 				catch(SQLException g)
 				{
 					System.err.print("SQLException: ");
 					System.err.println(g.getMessage());
-				}//close catch
-			}
+				}//  close catch
+			
+			}//  end RandomButton
 		
-		}
+		}//  end actionPerformed
 
-}//end class
+}//  end class
 
