e113fc26d8f01d557af55f8afd3fbaad7fe1852f
Ben Johnson <72088635+BenJohnson0@users.noreply.github.com>
2022-04-14 14:16:18 +0100

    Added some error checking
    Cleaned up the layout

---
+++ MyDataGUI_control.java
@@ -0,0 +1,11 @@
+package Data_Analysis;
+
+public class MyDataGUI_control 
+{
+	//main
+	public static void main(String[] args) 
+	{
+		@SuppressWarnings("unused")
+		MyDataGUI d = new MyDataGUI("Unemployment Rate of People aged 25-64 years");
+	}
+}
